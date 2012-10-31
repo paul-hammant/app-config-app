50e8fc1ec3ebc0d12e88d4e2c269f7b43b3d3a64
Paul Hammant <paul@hammant.org>
2012-10-31 17:54:58 -0500

    perforce launch script

---
+++ perforce_daemon.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DIR=$(cd $(dirname "$0"); pwd)
+
+mkdir -p perforce_files/root
+
+echo ${DIR}
+
+p4d -r ${DIR}/perforce_files/root -J ${DIR}/perforce_files/journal -L ${DIR}/perforce_files/p4err -p tcp:1666
