7d8d3a313a4158c958e598a59a9f495f4fde8c8a
Logan McGrath <lmcgrath@github.com>
2012-11-02 11:09:07 -0500

    useradd.rb now adds user and sets up working copy, submitting stack_configuration.json if not already in perforce

--- useradd.rb
+++ useradd.rb
@@ -4,23 +4,24 @@ def prompt(message)
 end
 
 arg = 0
-p4port = (ENV['P4PORT'] || ARGV[arg += 1] || (prompt 'p4port:')).strip
-user = (ARGV[arg += 1] || (prompt 'username:')).strip
-email = (ARGV[arg += 1] || (prompt 'email:')).strip
-password = (ARGV[arg += 1] || (prompt 'password:')).strip
+p4port = (ENV['P4PORT'] || (prompt 'p4port:')).strip
+user = (prompt 'username:').strip
+email = (prompt 'email:').strip
+password = (prompt 'password:').strip
 
-form  = "User:   #{user}" + '\n\n'
-form += "Email:  #{email}" + '\n\n'
+form  = "User: #{user}" + '\n\n'
+form += "Email: #{email}" + '\n\n'
 form += "Password: #{password}" + '\n\n'
 form += "FullName: #{user}" + '\n\n'
 
+puts "Creating user: #{user}"
 puts %x[echo "#{form}" | p4 -p #{p4port} user -i -f]
 
 working_copy = File.join File.expand_path(File.dirname(__FILE__)), 'wc', user
 client_name = user + 'Client'
 
 form  = "Client: #{client_name}" + '\n\n'
-form += "Owner:  #{user}" + '\n\n'
+form += "Owner: #{user}" + '\n\n'
 form += "Description:" + '\n'
 form += '\t' + "Created by #{user}" + '\n\n'
 form += "Root: #{working_copy}" + '\n\n'
@@ -28,7 +29,22 @@ form += "Options: noallwrite noclobber nocompress unlocked nomodtime normdir" + 
 form += "SubmitOptions: submitunchanged" + '\n\n'
 form += "LineEnd: local" + '\n\n'
 form += "View:" + '\n\n'
-form += '\t' + "//depot/... //testClient/..." + '\n\n'
+form += '\t' + "//depot/... //#{client_name}/..." + '\n\n'
 
+puts "Creating client: #{client_name}"
 puts %x[echo "#{form}" | p4 -p #{p4port} -u #{user} -P #{password} client -i]
 
+if (!Dir.exists? working_copy)
+  puts "Creating folder for working copy: #{working_copy}"
+  puts %x[mkdir -p #{working_copy}]
+end
+
+puts "Synchronizing working copy..."
+puts %x[p4 -p #{p4port} -u #{user} -P #{password} -c #{client_name} sync]
+
+if (Dir.entries working_copy).length == 2
+  puts "stack_configuration.json not under source control, adding it..."
+  puts %x[cp stack_configuration.json #{(File.join working_copy, 'stack_configuration.json')}]
+  puts %x[p4 -p #{p4port} -u #{user} -P #{password} -c #{client_name} add #{File.join working_copy, 'stack_configuration.json'}]
+  puts %x[p4 -p #{p4port} -u #{user} -P #{password} -c #{client_name} submit -d "Initial import of stack_configuration.json"]
+end
