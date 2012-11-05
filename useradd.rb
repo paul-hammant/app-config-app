a3eceb0b5ccf8e7d729b90c25411ccbcd6618842
Logan McGrath <lmcgrath@github.com>
2012-11-05 16:57:21 -0600

    remove existing user permissions to prevent too many getting written in

--- useradd.rb
+++ useradd.rb
@@ -3,7 +3,7 @@ require_relative 'admin'
 p4port = prompt 'p4port', ENV['P4PORT']
 user = prompt 'user'
 
-protect  = %x[p4 -p #{p4port} protect -o] + '\n'
+protect  = %x[p4 -p #{p4port} protect -o].gsub(/^.+\b#{user}\b.+$/, '') + '\n'
 protect += '\t' + "write user #{user} * //depot/app-config-app/..." + '\n\n'
 
 puts %x[echo "#{protect}" | p4 -p #{p4port} protect -i]
