8743bafe6f30a9db33bfe70d2baf1dd8bc50befb
Logan McGrath <lmcgrath@github.com>
2012-11-07 20:39:33 -0600

    got basic autosave working

--- stack_configuration.js
+++ stack_configuration.js
@@ -1,6 +1,9 @@
+$scope.dateRegExp = /\d\d?\/\d\d?\/\d\d\d\d/;
+
 $scope.newNick = function () {
     $scope.cfg.bannedNicks.push($scope.newNickname);
     $scope.newNickname = "";
+    $scope.change();
 };
 
 $scope.deleteNick = function (nick) {
@@ -9,4 +12,6 @@ $scope.deleteNick = function (nick) {
     angular.forEach(oldBannedNicks, function (n) {
         if (nick != n) $scope.cfg.bannedNicks.push(n);
     });
+    $scope.change();
 };
+
