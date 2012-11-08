c2e964840678352e7ef8e9be6384d0fb83066432
Logan McGrath <lmcgrath@github.com>
2012-11-07 18:27:42 -0600

    Added stack_configuration.html/js

---
+++ stack_configuration.js
@@ -0,0 +1,12 @@
+$scope.newNick = function () {
+    $scope.cfg.bannedNicks.push($scope.newNickname);
+    $scope.newNickname = "";
+};
+
+$scope.deleteNick = function (nick) {
+    var oldBannedNicks = $scope.cfg.bannedNicks;
+    $scope.cfg.bannedNicks = [];
+    angular.forEach(oldBannedNicks, function (n) {
+        if (nick != n) $scope.cfg.bannedNicks.push(n);
+    });
+};
