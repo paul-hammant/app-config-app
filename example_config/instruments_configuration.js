ddbfadcf5a4330a8c79c52602e7f28978e77469c
Logan McGrath <lmcgrath@github.com>
2012-11-20 08:47:06 -0600

    Added more instruments

--- example_config/instruments_configuration.js
+++ example_config/instruments_configuration.js
@@ -1,1 +1,18 @@
-$scope.instruments = [ {name: 'Piano'}, {name: 'Trumpet'}];
+$scope.instruments = [
+    {name: "Acoustic Grand Piano"},
+    {name: "Accordion"},
+    {name: "Harmonica"},
+    {name: "Violin"},
+    {name: "Trumpet"},
+    {name: "Trombone"},
+    {name: "Tuba"},
+    {name: "Muted Trumpet"},
+    {name: "Alto Sax"},
+    {name: "Oboe"},
+    {name: "Clarinet"},
+    {name: "Whistle"},
+    {name: "Banjo"},
+    {name: "Bag pipe"},
+    {name: "Bird Tweet"},
+    {name: "Applause"}
+];
